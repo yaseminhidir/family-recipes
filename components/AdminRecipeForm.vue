d18f9c2620e28edcd204d0b16b922d71d58476f8
yasemin <yaseminhidir@gmail.com>
2024-05-18 02:31:41 +0300

    no message

--- components/AdminRecipeForm.vue
+++ components/AdminRecipeForm.vue
@@ -7,6 +7,7 @@
             :counter="10"
             label="Title"
             hide-details
+            v-model="recipe.title"
             required
             variant="outlined"
           ></v-text-field>
@@ -14,19 +15,34 @@
         <v-col cols="12" md="3">
           <v-select
             clearable
-            chips
             label="Gruplar"
-            :items="['Aile Grubu', 'Arkadaş Grubu', 'Okul']"
+            :items="groups"
+            item-value="id"
+            item-title="name"
+            v-model="recipe.group_ids"
             multiple
             variant="outlined"
-          ></v-select>
+          >
+            <template v-slot:selection="{ item, index }">
+              <v-chip v-if="index < 1">
+                <span>{{ item.title }}</span>
+              </v-chip>
+              <span
+                v-if="index === 2"
+                class="text-grey text-caption align-self-center"
+              >
+                (+{{ recipe.group_ids.length - 1 }} others)
+              </span>
+            </template>
+          </v-select>
         </v-col>
         <v-col cols="12" md="3">
           <v-select
             clearable
             chips
             label="Kategori"
-            :items="['Çorba', 'Ana Yemek', 'Salata', 'Tatlı']"
+            :items="categories"
+            v-model="recipe.category"
             variant="outlined"
           ></v-select>
         </v-col>
@@ -35,15 +51,21 @@
             clearable
             chips
             label="Porsiyon"
-            :items="['1-2', '4-6', '8-10']"
+            :items="portions"
+            v-model="recipe.portion"
             variant="outlined"
           ></v-select>
         </v-col>
         <v-col cols="12" md="12">
-          <v-textarea label="Description" variant="outlined"></v-textarea>
+          <v-textarea
+            label="Description"
+            v-model="recipe.description"
+            variant="outlined"
+          ></v-textarea>
         </v-col>
         <v-col cols="12" md="12">
           <h2>Malzemeler</h2>
+          <p class="text-subtitle-2">Toplam Malzeme : {{ recipe.ingredients.length }}</p>
         </v-col>
         <v-col cols="12" md="12">
           <v-table height="300px" fixed-header>
@@ -52,35 +74,58 @@
                 <th class="text-left">Malzeme</th>
                 <th class="text-left">Ölçü</th>
                 <th class="text-left">Birim</th>
+                <th class="text-left">
+                  <v-btn
+                    @click="addIngredient"
+                    icon="mdi-plus"
+                    color="teal-lighten-3"
+                    variant="outlined"
+                  ></v-btn>
+                </th>
               </tr>
             </thead>
-            <tbody>
-              <tr>
-                <td>
+            <tbody class="mt-4">
+              <tr v-for="item in recipe.ingredients" :key="item.id">
+                <td width="30%" class="pa-3">
                   <v-text-field
+                    :autofocus="item.autofocus"
+                    :id="item.id"
                     label="Malzeme"
                     hide-details
                     required
                     variant="outlined"
+                    v-model="item.type"
                   ></v-text-field>
                 </td>
-                <td>
+                <td width="30%" class="pa-3">
                   <v-text-field
                     label="Ölçü"
                     hide-details
                     required
                     variant="outlined"
+                    v-model="item.measurement"
                   ></v-text-field>
                 </td>
-                <td>
+                <td width="30%" class="pa-3">
                   <v-select
                     clearable
                     hide-details
                     chips
                     label="Birim"
-                    :items="['1-2', '4-6', '8-10']"
+                    :items="units"
+                    v-model="item.unit"
                     variant="outlined"
                   ></v-select>
+                </td>
+                <td width="9%" class="pa-3">
+                  <v-btn
+                    @click="deleteIngredient(item.id)"
+                    color="red-lighten-3"
+                    variant="outlined"
+                   
+                    icon="mdi-trash-can"
+                  >
+                  </v-btn>
                 </td>
               </tr>
             </tbody>
@@ -91,29 +136,71 @@
   </v-form>
 </template>
 
-<script>
+<script setup>
+import { ref, nextTick } from "vue";
+
+const categories = ref(["Çorba", "Ana Yemek", "Salata", "Tatlı"]);
+const portions = ref(["1-2", "4-6", "8-10"]);
+const units = ref([
+  "Su bardağı",
+  "Çay bardağı",
+  "kase",
+  "Yemek kaşığı",
+  "Tatlı kaşığı",
+  "Çay Kaşığı",
+  "adet",
+  "gr",
+  "kg",
+  "ml",
+  "l",
+  "tutam",
+  "demet",
+]);
+const groups = ref([
+  { id: 1, name: "Aile Grubu" },
+  { id: 2, name: "Arkadaş Grubu" },
+  { id: 3, name: "Okul" },
+]);
 const recipe = ref({
+  id:1,
   title: "Tarhana Çorbası",
   ingredients: [
-    { measurement: "2", unit: "Yemek kaşığı", type: "Toz tarhana" },
+    { id: 1, measurement: "2", unit: "Yemek kaşığı", type: "Toz tarhana" },
     {
+      id: 2,
       measurement: "1",
       unit: "Yemek kaşığı",
       type: "Toz tarhana",
       description: "Su eklendikten sonra ilave edilecek",
     },
-    { measurement: "1", unit: "Yemek kaşığı", type: "Tereyağı" },
-    { measurement: "1", unit: "Çay kaşığı", type: "Sıvı yağ" },
-    { measurement: "1", unit: "Yemek kaşığı", type: "Salça" },
-    { measurement: "5", unit: "Su Bardağı", type: "Sıcak Su" },
+    { id: 3, measurement: "1", unit: "Yemek kaşığı", type: "Tereyağı" },
+    { id: 4, measurement: "1", unit: "Çay kaşığı", type: "Sıvı yağ" },
+    { id: 5, measurement: "1", unit: "Yemek kaşığı", type: "Salça" },
+    { id: 6, measurement: "5", unit: "Su Bardağı", type: "Sıcak Su" },
   ],
   category: "Çorba",
   image:
     "https://www.kevserinmutfagi.com/wp-content/uploads/2008/11/tarhana_corbasi2-1.jpg",
   description: "",
   portion: "4-6",
-  user_id: user.uid,
-  username: user.displayName,
-  group_ids: [1, 2],
+  user_id: 1,
+  username: "yasemin",
+  group_ids: [1, 2, 3],
 });
+
+async function addIngredient() {
+  const id = Math.floor(Math.random() * 999999);
+  recipe.value.ingredients.push({ autofocus: true, id });
+  await nextTick(); // ui update edildi.
+
+  let input = document.getElementById(id);
+  input.scrollIntoView({
+    behavior: "smooth",
+  });
+
+
+}
+function deleteIngredient(id){
+    return recipe.value.ingredients=recipe.value.ingredients.filter(x=>x.id != id )
+  }
 </script>
